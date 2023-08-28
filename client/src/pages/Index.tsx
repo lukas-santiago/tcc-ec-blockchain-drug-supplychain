44f4fc83c298e5a61bbfb6238967f8b0d2b217d9
lukas-santiago <lukas.asant@gmail.com>
2023-08-28 20:08:13 -0300

    Basic Layout + emotion/react fix

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -1,7 +1,12 @@
+import { Container, Grid } from '@mui/material'
+
 export function Index() {
   return (
     <div>
       <h3>Welcome Home!</h3>
+      <Container>
+        <Grid>sd</Grid>
+      </Container>
     </div>
   )
 }
--- client/src/pages/Root.tsx
+++ client/src/pages/Root.tsx
@@ -1,13 +1,46 @@
+import { css } from '@emotion/react'
+import { Paper, Stack, styled } from '@mui/material'
 import { Outlet, Link } from '@tanstack/react-router'
+
+const Item = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  margin: theme.spacing(1),
+  color: theme.palette.text.secondary,
+}))
 
 export function Root() {
   return (
-    <>
-      <div>
-        <Link to="/">Home</Link> <Link to="/about">About</Link>
+    <div
+      css={css`
+        display: flex;
+        height: 100vh;
+      `}
+    >
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          width: 200px;
+          height: 100%;
+        `}
+      >
+        <Stack>
+          <Item>
+            <Link to="/">Home</Link>
+          </Item>
+          <Item>
+            <Link to="/about">About</Link>
+          </Item>
+        </Stack>
       </div>
-      <hr />
-      <Outlet />
-    </>
+      <div
+        css={css`
+          flex: 1;
+        `}
+      >
+        <Outlet />
+      </div>
+    </div>
   )
 }
