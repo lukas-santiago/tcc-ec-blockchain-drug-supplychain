edf316971988c3d18491bdcf4dd3e04fa8595378
Lukas A. Santiago <lukas.asant@gmail.com>
2023-11-27 04:15:23 -0300

    ...

--- dapp-web/src/pages/platform/PlatformHome.jsx
+++ dapp-web/src/pages/platform/PlatformHome.jsx
@@ -2,7 +2,7 @@ export function PlatformHome() {
   return (
     <>
       <h1 className="mb-4">Plataforma</h1>
-      <p>Escolha um serviço no menu ao lado para começar</p>
+      <p>Conecte uma carteira e escolha um serviço no menu ao lado para começar</p>
     </>
   );
 }
